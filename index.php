adba1479fa75d48833a04f040142463316f711a0
Gianluca Sirianni <gianluca.sirianni7@gmail.com>
2023-03-22 02:47:55 +0100

    style del register

--- index.php
+++ index.php
@@ -6,44 +6,58 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edusogno</title>
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css'integrity='sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==' crossorigin='anonymous' referrerpolicy='no-referrer'>
     <link rel="stylesheet" href="./assets/styles/style.css">
 </head>
 
 <body>
 
-    <header>
-        
+    <header class="nav">
+        <div class="logo">
+            <img src="./assets/img/logo-edusogno.PNG" alt="">
+        </div>
     </header>
 
-    <div class="register-form">
-        <form action="register.php" method="POST">
-            <div class="input-field">
-                <label for="firstname">Nome:</label>
-                <input type="text" name="firstname" id="firstname" style="width: 300px;" placeholder="Inserisci il tuo nome" required>
-            </div>
-            <div class="input-field">
-                <label for="lastname">Cognome:</label>
-                <input type="text" name="lastname" id="lastname" style="width: 300px;" placeholder="Inserisci il tuo cognome" required>
-            </div>
-            <div class="input-field">
-                <label for="email">E-mail:</label>
-                <input type="email" name="email" id="email" style="width: 300px;" placeholder="Inserisci la tua E-mail" required>
-            </div>
-            <div class="input-field">
-                <label for="password">Password:</label>
-                <input type="password" name="password" id="password" style="width: 300px;" placeholder="Inserisci la tua password" required>
-            </div>
-            <div class="submit-btn">
-                <input type="submit" value="Registrati">
-            </div>
-            <p>se hai gia' un account vai al <a href="login.html">Login</a></p>
+    <main>
+        <div class="create">
+            <h1>Crea il tuo account</h1>
+        </div>
+        <div class="register-form">
+            <div class="form-container">  
+                <form action="register.php" method="POST">
+                    <div class="input-field">
+                        <label for="firstname">Inserisci il nome</label>
+                        <input type="text" name="firstname" id="firstname" placeholder="Mario" required>
+                    </div>
+                    <div class="input-field">
+                        <label for="lastname">Inserisci il cognome</label>
+                        <input type="text" name="lastname" id="lastname"  placeholder="Rossi" required>
+                    </div>
+                    <div class="input-field">
+                        <label for="email">Inserisci l'email:</label>
+                        <input type="email" name="email" id="email" placeholder="name@example.com" required>
+                    </div>
+                    <div class="input-field">
+                        <label for="password">Inserisci la password</label>
+                        <div class="relative">
+                            <input type="password" name="password" id="password"  placeholder="Scrivila qui" required>
+                            <i class="fa-solid fa-eye" id="togglePassword"></i>
+                        </div>
 
-        </form>
+                    </div>
+                    <div class="submit-btn">
+                        <input id="btn" type="submit" value="Registrati">
+                    </div>
+                    <div class="text-center">
+                        <p class="small"><a href="login.html">Hai già un account? <strong>Accedi</strong> </a></p>
+                    </div>
+                </form>
+            </div> 
+        </div>
+    </main>
 
-    </div>
 
-
-
+    <script type="text/javascript" src="./assets/js/script.js"></script>
 </body>
 
 </html>
